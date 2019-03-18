bf9255c23518ec063b17fb277bce3b1c394800a5
nroldanf <33168121+nroldanf@users.noreply.github.com>
2019-03-18 13:00:14 -0500

    Update WaveletDenoising.m
    Generalización para levels niveles de descomposición

--- WaveletDenoising.m
+++ WaveletDenoising.m
@@ -1,13 +1,13 @@
 function m_sigNew = WaveletDenoising(m_sig,wave,thr,levels,fs,plotFlag)
 % Realiza un filtro wavelet con una wavelet madre wave y niveles de umbral
-% thr para cada banda en orden: Dlevels hasta la aproximación Alevels
+% thr para cada banda en orden: Dlevels hasta la aproximaciÃ³n Alevels
 
 
 
-% Parámetros importantes
+% ParÃ¡metros importantes
 s_length = size(m_sig,1);% en filas
 s_chann = size(m_sig,2);% en columnas
-m_sigNew = m_sig;% matriz de la nueva señal
+m_sigNew = m_sig;% matriz de la nueva seÃ±al
 t = (0:s_length-1)/fs;% vector de tiempo
 f = linspace(0,fs/2,length(fftMag(m_sig(:,1))));
 
@@ -15,10 +15,10 @@ bands = {'Ruido','\gamma (25-100 Hz)','\beta (14-35 Hz)','\alpha (8-14 Hz)','\th
 for i = 1:s_chann
 % Decomposition
     [c0,l0] = wavedec(m_sig(:,i),levels,wave);
-    D = zeros(6,s_length);
-    % Umbralización (filtrado)
+    D = zeros(levels+1,s_length);
+    % UmbralizaciÃ³n (filtrado)
     for j = 1:levels
-        % Para el nivel de coeficientes de aproximación alevels
+        % Para el nivel de coeficientes de aproximaciÃ³n alevels
         if j == levels
             D(j,:) = wrcoef('d',c0,l0,wave,j);
             D(j,:) = wthresh(D(j,:),'s',thr(j));
@@ -33,20 +33,20 @@ for i = 1:s_chann
     if plotFlag == 1 
     % Plotee cada subbanda
         figure('Name',['Canal ' num2str(i)],'NumberTitle','off')
-        for k = 1:6
+        for k = 1:levels+1
             %tiempo
-            subplot(6,2,k*2-1);
+            subplot(levels+1,2,k*2-1);
             plot(t,D(:,k));grid on;
             xlabel('Tiempo');ylabel('\mu V');
             %frecuencia
-            subplot(6,2,k*2);
+            subplot(levels+1,2,k*2);
             plot(f,fftMag(D(:,k)));grid on;
             xlabel('Frecuencia')
             title(bands{k})
         end
     end
 % Sume los componentes
-    for k = 1:6
+    for k = 1:levels+1
         m_sigNew(:,i) = m_sigNew(:,i)+D(:,k);
     end
 end
